c89eb6cf3fb9838235fe3395adeafb3dc84cd53c
Devgoga16 <mgdammy15@gmail.com>
2025-04-03 19:39:50 -0500

    agregando nuevos campos en servicios list

--- Models/Response/ServicioResponseList.cs
+++ Models/Response/ServicioResponseList.cs
@@ -5,10 +5,15 @@ namespace almacen_samplag.Models.Response
     public class ServicioResponseList
     {
         public int idServicio { get; set; }
-        public int idCliente { get; set; }
-        public string? nombreCliente { get; set; }
         public string? descripcionServicio { get; set; }
         public DateTime fecha { get; set; }
+        public int idCliente { get; set; }
+        public string? nombreCliente { get; set; }
+        public int idSede {  get; set; }
+        public string? nombreSede { get; set; }
+        public string? direccionSede { get; set; }
+        public string? rucSede { get; set; }
+
         [NotMapped]
         public List<Colaborador>? colaboradores { get; set; }
     }
